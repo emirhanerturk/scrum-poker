a4bda3ed9459b90a249299769fe73ef6ac2ce759
Emirhan Erturk <emirhan@rapsodo.com>
2021-03-31 23:59:18 +0300

    added pm2.json file

--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
+import { environment } from "../environments/environment";
+
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to backend!' };
+  getData(): { message: string, production: boolean } {
+    return { message: 'Welcome to backend!', production: environment.production };
   }
 }
